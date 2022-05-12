a91ef7d8d708dfd4d0472571d0becf281d44f592
SHAI GUNDERSEN <shai.gundersen@gmail.com>
2022-05-12 14:23:55 +0300

    another commit :)

--- main.py
+++ main.py
@@ -1,127 +1,130 @@
-import random
-
+from solution import *
+import matplotlib.pyplot as plt
 UP = '^'
 RIGHT = '>'
 LEFT = '<'
 SPACE = ' '
 
-def print_grid(mat, constraints):
-    l = len(mat)
-    print(2 * l * "*" + "*")
-    for i in range(len(mat)):
-        print("|", end='')
-        for j in range(len(mat[i])):
-            # for big, small in constraints:
-            #     b_i, b_j = big
-            #     s_i, s_j = small
 
-            if j == len(mat[i]) - 1:
-                print(mat[i][j], end='|\n')
-            else:
-                print(mat[i][j], end=' ')
-    print(2 * l * "*" + "*")
 
 def parse_input(io):
-    dic = {'N': 3}
+    dic = {}
+    with open(io, 'r') as f:
+        lines = f.readlines()
+        lines = [line.replace('\n', '') for line in lines]  # remove all new-line
+        i = 0
+        dic['N'] = int(lines[i])
+        i += 1
+        mat_init_size = int(lines[i])
+        i += 1
+        dic['mat_init'] = []
+        for j in range(mat_init_size):
+            line = lines[i + j]
+            splitted = line.split(' ')
+            splitted = tuple([int(num) for num in splitted])
+            dic['mat_init'].append(splitted)
+        i += mat_init_size
+        greater_init_size = int(lines[i])
+        i += 1
+        dic['constraints'] = []  # each constraint is a tuple of 2 (i,j) indexes, first is bigger than second
+        for j in range(greater_init_size):
+            line = lines[i + j]
+            splitted = line.split(' ')
+            splitted = [int(num) for num in splitted]
+            big = (splitted[0], splitted[1])
+            small = (splitted[2], splitted[3])
+            dic['constraints'].append((big, small))
+
     return dic
 
-def make_grid(N):
-    g = []
-    for i in range(N + N - 1):
-        # even row
-        if i % 2 == 0:
-            row = []
-            j = 0
-            while j < N - 1:
-                row.append(j)
-                r = random.randint(0, 2)
-                if r % 2 == 0:
-                    row.append(LEFT)
-                else:
-                    row.append(SPACE)
-                j += 1
-            row.append(j)
-        else:
-            # odd row
-            row = []
-            col = 0
-            while col < N:
-                if col % 2 == 0:
-                    row.append(UP)
-                else:
-                    row.append(SPACE)
-                col += 1
-            row.append(SPACE)
-
-
-        g.append(row)
-    return g
-
-def make_grid2(N):
-    c = 0
-    g = []
-    for i in range(game_dict['N']):
-        row = []
-        for j in range(game_dict['N']):
-            row.append(c)
-            c += 1
-        g.append(row)
-    return g
-
-
-def get_score(grid):
-    pass
-
-def eval_function(grid, constraints):
-    count = 0
-    for big, small in constraints:
-        if not (grid[big[0] - 1][big[1] - 1] > grid[small[0] - 1][small[1] - 1]):
-            count += 1
-    return count
-
-def mutate(grid, N):
-    """ returns a new grid where two elements from a random row are swapped """
-    import copy
-    temp_grid = copy.deepcopy(grid)
-    row = random.randint(0, N - 1)
-    index = random.sample(range(N), 2)
-    temp = temp_grid[row][index[0]]
-    temp_grid[row][index[0]] = temp_grid[row][index[1]]
-    temp_grid[row][index[1]] = temp
-    return temp_grid
-
-def crossover(grid1, grid2, N):
-    """
-    picks a random row - R.
-    Append to grid3 all rows from grid1 until R and all rows from grid2 from R until the end
-    """
-    row = random.randint(0, N)
-    grid3 = grid1[:row]
-    grid3 += grid2[row:]
-    return grid3
-
-class SolutionFactory:
-    def __init__(self):
-        pass
-
-    def get_solution(self):
-        return {}
 
 class StrategySolver:
     def __init__(self):
         pass
 
+
+class GeneticAlgo(ABC):
+    def __init__(self, population_size, solution_factory: SolutionFactory, constraints):
+        self.population_size = population_size
+        self.population = [solution_factory.generate_solution() for i in range(population_size)]
+        self.constraints = constraints
+        self.found_sol = False
+        self.best_sol = None
+
+    def run(self):
+        best_score = -1
+        generations_scores = []
+        gen = 1
+        while not self.found_sol:
+            # eval sols
+            scores = self.eval_population()
+            cur_best = max(scores)
+            if cur_best > best_score:
+                best_score = cur_best
+            if gen % 100 == 0:
+                print("------------------------------")
+                print(f'gen -> {gen}')
+                print(f'best score -> {best_score}')
+                print(f'avg score -> {sum(scores)/len(scores)}')
+            generations_scores.append(scores)
+            weighted_population = self.create_weighted_population(scores)
+            self.create_new_population(weighted_population)
+            gen += 1
+
+    def create_new_population(self, weighted_population):
+        new_pop = []
+        for i in range(self.population_size):  # generate new population with fixed size
+            parents = random.sample(weighted_population, 2)
+            sol_A, sol_B = parents[0], parents[1]
+            sol_A = sol_A.mutate(0.2)
+            sol_B = sol_B.mutate(0.2)
+            child_sol = sol_A.crossover(sol_B)
+            new_pop.append(child_sol)
+        self.population = new_pop
+
+    def create_weighted_population(self, scores):
+        weighted_population = []
+        for i, solution in enumerate(self.population):
+            for j in range(scores[i]):
+                weighted_population.append(solution)
+        return weighted_population
+
+    def eval_population(self):
+        scores = []
+        for solution in self.population:
+            score = solution.fitness()
+            # score = self._eval_solution(solution)
+            if score == (len(self.constraints) + len(solution)*len(solution)):  # all constraints are met
+                self.found_sol = True
+                self.best_sol = copy.deepcopy(solution)
+            scores.append(score)
+        return scores
+
+    def _eval_solution(self, solution):
+        """ returns number of satisfied constraints """
+        count = 0
+        for constraint in self.constraints:
+            if solution.satisfy(constraint):
+                count += 1
+
+        count += solution.valid()
+        return count
+
+
+
 if __name__ == '__main__':
-    game_dict = parse_input(io=None)
-    n = game_dict['N']
-    grid = make_grid2(n)
-    # g = [[0 if (i % 2 == 0 and j % 2 == 0) else ' ' for j in range(n + n -1)] for i in range(n + n - 1)]
-    # g = [[j for j in range(n)] for i in range(n)]
-    constraint = [((1, 1), (1, 2))]
-    # print_grid(grid, constraint)
-    new_grid = mutate(grid, n)
-    print_grid(grid, constraint)
-    print_grid(new_grid, constraint)
-    new_new_grid = crossover(grid, new_grid, n)
-    print_grid(new_new_grid, constraint)
+    # init_vect = sum([range(1, 10)] * 9, [])
+    m = [[1,2,3],[4,5,6],[7,8,9]]
+    print(m)
+    print(list(zip(*m)))
+    #
+    # input_dict = parse_input(io='./input.txt')
+    # n = input_dict['N']
+    # msf = MatrixSolutionFactory(n, input_dict['mat_init'])
+    # constraints = input_dict['constraints']
+    # ga = GeneticAlgo(100, msf, constraints)
+    # ga.run()
+    # if ga.best_sol:
+    #     ga.best_sol.print_solution()
+
 
---
+++ solution.py
@@ -0,0 +1,163 @@
+import random
+import copy
+from abc import ABC, abstractmethod
+import math
+
+class Solution(ABC):
+    @abstractmethod
+    def mutate(self, percent):
+        pass
+
+    @abstractmethod
+    def crossover(self, solution: 'Solution'):
+        pass
+
+    @abstractmethod
+    def print_solution(self):
+        pass
+
+    @abstractmethod
+    def satisfy(self, constraint):
+        pass
+
+    @abstractmethod
+    def __len__(self):
+        pass
+
+    @abstractmethod
+    def valid(self):
+        pass
+
+    @abstractmethod
+    def fitness(self):
+        pass
+
+
+
+class MatrixSolution(Solution):
+    def __init__(self, N, seeds):
+        self.__N = N
+        self.__solution = []
+        self.__seeded_solution = [[0 for j in range(N)] for i in range(N)]
+        for seed in seeds:
+            i, j, num = seed
+            self.__seeded_solution[i - 1][j - 1] = num
+
+        base_permutation = list(range(1, N + 1))  # create list of [1,2,..,N]
+        for i in range(N):
+            base_permutation_copy = copy.deepcopy(base_permutation)
+            random.shuffle(base_permutation_copy)
+            self.__solution.append(base_permutation_copy)
+        self._conform_seed()  # initial solution has to conform with seed
+
+
+    def _conform_seed(self):
+        """ make this solution conform with given seed """
+        N = len(self.__solution)
+        for i in range(N):
+            for j in range(N):
+                if self.__seeded_solution[i][j] != 0 and self.__solution[i][j] != self.__seeded_solution[i][j]:
+                    # swap places with where seed is supposed to be
+                    idx = self.__solution[i].index(self.__seeded_solution[i][j])
+                    temp = self.__solution[i][j]
+                    self.__solution[i][j] = self.__solution[i][idx]
+                    self.__solution[i][idx] = temp
+
+    def mutate(self, percent):
+        """
+        picks a percent of rows to mutate
+        for each picked row, swap two random elements
+        :return copy of current Solution after swapping
+        """
+        sol_copy = copy.deepcopy(self)
+        N = len(self.__solution)
+        rows_to_mutate = math.ceil(N * percent)
+        rows = random.sample(range(N), rows_to_mutate)
+        for row in rows:
+            cols = random.sample(range(N), 2)  # pick two indexes from that row to be swapped
+            temp = sol_copy.__solution[row][cols[0]]
+            sol_copy.__solution[row][cols[0]] = sol_copy.__solution[row][cols[1]]
+            sol_copy.__solution[row][cols[1]] = temp
+        sol_copy._conform_seed()
+        return sol_copy
+
+    def crossover(self, solution: 'MatrixSolution'):
+        """
+        picks a random row - R.
+        Append to grid3 all rows from grid1 until R and all rows from grid2 from R until the end
+        """
+        sol_copy = copy.deepcopy(self)
+        N = len(self.__solution)
+        row = random.randint(0, N - 1)
+        new_sol = self.__solution[:row]
+        new_sol += solution.__solution[row:]
+        sol_copy.__solution = new_sol
+        sol_copy._conform_seed()
+        return sol_copy
+
+    def print_solution(self):
+        N = len(self.__solution)
+        print(2 * N * "*" + "*")
+        for i in range(N):
+            print("|", end='')
+            for j in range(N):
+                if j == N - 1:
+                    print(self.__solution[i][j], end='|\n')
+                else:
+                    print(self.__solution[i][j], end=' ')
+        print(2 * N * "*" + "*")
+
+    def __consistent(self, solution):
+        """ returns num of diff elements in each row. Best case is 0 when each row is a permutation """
+        return sum(self.__N - len(set(row)) for row in solution)
+
+    def __compare_to_seed(self):
+        """ count number of mismatches between initial seeded solution with current solution """
+        count = 0
+        for seed_row, row in zip(self.__seeded_solution, self.__solution):
+            for x_sr, x_r in zip(seed_row, row):
+                if x_sr:  # when x_sr == 0 -> no seed
+                    if x_sr != x_r:  # mismatch
+                        count += 1
+        return count
+
+    def fitness(self):
+        # calc diff elements in each ROW
+        fit = self.__consistent(self.__solution)
+        # calc diff elements in each COL
+        fit += self.__consistent(zip(*self.__solution))
+        # calc diff from seed (given digits)
+        fit += self.__compare_to_seed()
+        return fit
+
+    def satisfy(self, constraint):
+        big, small = constraint
+        return self.__solution[big[0] - 1][big[1] - 1] > self.__solution[small[0] - 1][small[1] - 1]
+
+    def valid(self):
+        """ count number of distinct elements in a col """
+        N = len(self.__solution)
+        count = 0
+        for j in range(N):
+            s = set([])
+            for i in range(N):
+                s.add(self.__solution[i][j])
+            count += len(s)
+        return count
+
+    def __len__(self):
+        return len(self.__solution)
+
+class SolutionFactory(ABC):
+    @abstractmethod
+    def generate_solution(self) -> Solution:
+        pass
+
+
+class MatrixSolutionFactory(SolutionFactory):
+    def __init__(self, N, seed):
+        self.__matrix_size = N
+        self.__seed = seed
+
+    def generate_solution(self) -> MatrixSolution:
+        return MatrixSolution(self.__matrix_size, self.__seed)
